b84d0e85ca64dd5cbc5db05675230e3b45a81201
NandanXDm <nanven9@gmail.com>
2024-05-05 15:31:04 +0530

    Added Favicon

--- src/components/SearchJobs/SearchJobs.jsx
+++ src/components/SearchJobs/SearchJobs.jsx
@@ -1,17 +1,18 @@
 import useSearchJobs from "../../utls/useSearchJobs"
 import JobCardsContainer from "./JobCardsContainer/JobCardsContainer";
 import SearchFilters from "./SearchFilters/SearchFilters";
+import './SearchJobs.css'
 
 const SearchJobs = () => {
 
   const {data,error,loading}=useSearchJobs();
 
   return (
-    <>
-      <div>Search Jobs</div>
+    <div className="search-jobs">
+      <h1>Search Jobs</h1>
       <SearchFilters/>
       <JobCardsContainer jdList={data}/>
-    </>
+    </div>
   )
 }
 
